9ed68a2cf40dc2b1a31021bd874ba96b0f4f71fc
Miftahul Huda <miftahul.huda@devoteam.com>
2021-07-02 00:39:01 +0700

    add show bounding box

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask import request
 from flask import json
 from flask.wrappers import Request
-from ocr import parse, parse_to_image
+from ocr import parse, parse_to_image, image_boxes
 from six.moves import urllib
+import os
 app = Flask(__name__)
 
 from flask import send_file
@@ -30,7 +31,16 @@ def ocr2image():
     filename = parse_to_image(url)
     return send_file(filename,  mimetype='image/png')
 
+@app.route('/image-rect', methods=['GET', 'POST'])
+def imagerect():
+    url = request.args.get("url")
+    content = request.json
+    filename = image_boxes(url, content["positions"])
+    #url = urllib.parse.unquote(url)
+    print(url)
+    return send_file(filename,  mimetype='image/png')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=os.environ['APPLICATION_PORT'])
 
 
--- ocr.py
+++ ocr.py
@@ -2,12 +2,16 @@ import cv2
 import pytesseract
 from pytesseract import Output
 from urllib.request import urlopen
+from urllib.parse import quote
 import numpy as np
 
 
 def parse(imageUrl):
 
     img = url_to_image(imageUrl)
+
+    img = get_grayscale(img)
+    # img = thresholding(img)
 
     d = pytesseract.image_to_data(img, output_type=Output.DICT)
     print(d.keys())
@@ -41,12 +45,78 @@ def parse_to_image(imageUrl):
 
     return filename
 
+def image_boxes(imageUrl, positions):
+
+    img = url_to_image(imageUrl)
+
+    print(positions)
+    for position in positions:
+        img = cv2.rectangle(img, (position['x'], position['y']), (position['x'] + position['w'], position['y'] + position['h']), (0, 255, 0), 2)
+    
+    filename = "/tmp/temp.png";
+    cv2.imwrite(filename, img)
+
+    return filename
+
 def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
     # download the image, convert it to a NumPy array, and then read
     # it into OpenCV format
+    #url = quote(url.encode("utf-8"), safe='')
+    url = url.replace(' ', '%20')
+    print("url")
+    print(url)
     resp = urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, readFlag)
 
     # return the image
     return image
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.medianBlur(image,5)
+ 
+#thresholding
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#dilation
+def dilate(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+    
+#erosion
+def erode(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
